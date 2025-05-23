3d88b5d4adc648c2a48380de66de33da7c81bfaf
sajid33 <mdeditz0786@gmail.com>
2025-05-23 12:59:47 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -283,28 +283,22 @@ export const education = [
 export const projects = [
   {
     id: 9,
-    title: "Trackify",
-    date: "Jun 2023 - Jul 2023",
+    title: "Students ID Crad Generator",
+    date: "Aug 2024 - Mar 2025",
     description:
-      "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
+      "This web application allows educational institutions to generate and manage digital student ID cards efficiently. The platform provides a user-friendly interface to input student details, upload photos, and instantly generate printable ID cards.",
     image:
-      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+      "http://idoxd69.sufydely.com/ID.JPG",
     tags: [
-      "Docker",
-      "AWS",
-      "DuckDNS",
-      "Eslint",
-      "Husky",
-      "CI/CD",
+      "Next.js",
       "React Js",
       "MongoDb",
       "Node Js",
       "Express Js",
-      "Redux",
     ],
     category: "web app",
-    github: "https://github.com/rishavchanda/Trackify",
-    webapp: "https://trackify.duckdns.org",
+    github: "https://tinyurl.com/2wdf76wm",
+    webapp: "https://tinyurl.com/w442e5r4",
   },
   {
     id: 0,
